aaf115fc512a26048ef4ba4532e992aaf01efa19
cangaco05 <cyber.cangaco@gmail.com>
2022-10-13 21:35:44 -0300

    Add primeira parte da aula

---
+++ js/main.js
@@ -0,0 +1,50 @@
+
+var d = new Date();
+alert(d.getMinutes());
+alert(d.getHours());
+//alert(d);
+
+/*var count;
+for(count=0; count <= 5; count++){
+    alert(count);
+};
+*/
+
+/*var count =0;
+while (count <=5){
+    console.log(count);
+    alert(count);
+    count ++;
+};
+*/
+
+/*var idade = prompt("Qual sua idade?19")
+var idade = 18;
+if (idade >= 18) {
+    alert("maior de idade")
+}else{
+    alert("menor de idade")
+};
+*/
+
+/* var fruta = {nome:"maça", cor: "vermelha"}
+console.log(fruta);
+
+var lista = ["maça", "pêra", "laranja" ];
+lista.push("uva")
+console.log(lista[1])
+alert(lista[1])
+
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join(" | "))
+
+var nome = "Amaury Prats";
+var n1 = 3;
+var n2 = 5;
+var frase = "Japão é melhor time do mundo";
+alert(nome + " tem " + idade +" anos ");
+console.log(nome);
+console.log(n1 * n2);
+console.log(frase.toLowerCase());
+*/
